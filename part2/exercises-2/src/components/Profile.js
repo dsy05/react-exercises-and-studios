c06e23bf559eed11669b42c176df20e9a1735fa0
Daniyal Syed <daniyal.syed@live.com>
2023-11-12 00:19:06 -0600

    react part 2 exercises

--- part2/exercises-2/src/components/Profile.js
+++ part2/exercises-2/src/components/Profile.js
@@ -1,14 +1,28 @@
 import './styles.css';
 import Button from './Button';
+import oceans from './oceans.json'
 
-
-function Profile()
-{
-   return(
-      <>
-         <h3>Coming Soon! Profiles of Ocean creatures</h3>
+const listItem = oceans.map(ocean => //declared listItem to equal ocean.map set ocean as the callback parameter
+   <div key={ocean.id} className={`${ocean.fishCheck === "true" ? "isAFish" : "profile"}`}>
+      <h1>{ocean.name}</h1>
+      <img src={ocean.image} alt={ocean.name} className='img'/>
+      <h2>Fun Facts</h2>
+         <ul>
+            <li>{ocean.fact1}</li>
+            <li>{ocean.fact2}</li>
+            <li>{ocean.fact3}</li>
+         </ul>
          <Button />
-      </>  
+   </div>
+); // each profile contained in the div since working with json object uisng curly braces
+
+function Profile() {
+   
+
+   return(
+      <ul>
+         {listItem}
+      </ul> 
    );
 }
 
