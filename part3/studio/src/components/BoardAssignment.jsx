41583a4f9ead501b9780c3b264f0c28dc1af35c1
Daniyal Syed <daniyal.syed@live.com>
2023-11-16 22:32:45 -0600

    react part 3 studio

--- part3/studio/src/components/BoardAssignment.jsx
+++ part3/studio/src/components/BoardAssignment.jsx
@@ -1,15 +1,28 @@
 import { useState } from 'react';
 
 export default function BoardAssignment () {
-   const boards = [];
+   const boards = [
+      {label: "Appetizer", value: "Appetizer"},
+      {label: "Tea Time", value: "Tea Time"},
+      {label: "Brunch", value: "Brunch"},
+   ];
+
+   let boardOptions = boards.map((board) => {
+      return <option key={board.value}>{board.label}</option>
+   });
+
+
+   const [boardName, setBoardName] = useState('no boards yet!');
 
    const handleChange = (event) => {
+      setBoardName(event.target.value);
    }
 
    return (
       <div style={{paddingTop: "50px"}}>
       <label>Save to Board: </label>
       <select value={boardName} onChange={handleChange}>
+         {boardOptions}
       </select>
 
       <p>Saved to {boardName}!</p>
--- part3/studio/src/components/Recipe.jsx
+++ part3/studio/src/components/Recipe.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react';
 
 const RecipeAuthor = () => {
-   let authorLink = "";
-   let authorPhoto = "";
-   let authorName = "";
+   let authorLink = "https://twoclovesinapot.com/get-to-know-us/";
+   let authorPhoto = "https://twoclovesinapot.com/wp-content/uploads/2018/12/dsc_4064-2-1-1024x681.jpg";
+   let authorName = "Sarah Mirza & Vaquas Saleem";
 
    return (
       <div>
-         <img src={authorPhoto} alt = "" style={{objectFit: "contain", borderRadius: "50%"}} />
+         <img src={authorPhoto} alt = "" style={{objectFit: "contain", borderRadius: "50%", width: "75%"}} />
          <div>
             <h3>{authorName}</h3>
             <a href={authorLink}></a> 
@@ -17,16 +17,22 @@ const RecipeAuthor = () => {
 }
 
 const RecipeIngredients = () => {
-   const ingredients = [];
+   const ingredients = 
+   ["Yogurt mixture- Yogurt and sugar mix them together", 
+   "Chutneys- Khatti Meethi and Green Chutney",
+   "Potatoes and chickpea mixture- This mixture consist of chickpeas and potatoes along with seasonings and fresh cilantro.",
+   "Sev- Dried fried South Asian noodle bits.",
+   "Papdi - They are little circular shape salty bits."];
+
+   let ingredientListItems = ingredients.map((ingredient, index) => {
+      return <li key={index}>{ingredient}</li>;
+   });
+
    return(
       <div>
          <h3>Recipe Ingredients</h3>
          <ul>
-            <li>{ingredients[0]}</li>
-            <li>{ingredients[1]}</li>
-            <li>{ingredients[2]}</li>
-            <li>{ingredients[3]}</li>
-            <li>{ingredients[4]}</li>
+            {ingredientListItems}
          </ul>
       </div>
    );
@@ -36,8 +42,8 @@ const RecipeDescription = () => {
    return (
       <div> 
          <div>
-            <h1></h1>
-            <p></p>
+            <h1>Golgappa Chaat</h1>
+            <p>Golgappa Chaat is stuffing crispy puffed puris with seasoned chickpeas and potatoes and topped with Chutneys, fried noodle bits, and sweet yogurt mixture.</p>
          </div>
          <div className="recipePhotoBlock">
             <RecipeIngredients />
@@ -49,7 +55,7 @@ const RecipeDescription = () => {
 
 const RecipePhoto = () => {
    return (
-      <img src="" alt="" className="imageUpdates"/>
+      <img src="https://twoclovesinapot.com/wp-content/uploads/2022/12/IMG_0901_jpg-768x1024.jpg" alt="GOLGAPPAY" className="imageUpdates" height="600px"/>
    );
 }
 
